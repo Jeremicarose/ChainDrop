fd346533dd28600d8d68c6336087e859f7cb5495
Jeremicarose <arosejeremic5@gmail.com>
2026-01-07 23:45:00 +0300

    Minor update: 2026-01-07 23:45:00 - Added 1 file(s) - Modified 1 file(s)
    Changed files:
     M frontend/src/App.jsx
    ?? frontend/src/pages/

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import HomePage from './pages/Homepage';
 import ClaimPage from './pages/ClaimPage';
-import SendPage from './pages/SendPage';
+import SendPage from './pages?sendPage';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/"element={<HomePage />} />
         <Route path="/claim/:claimToken" element={<ClaimPage />} />
         <Route path="/send" element={<SendPage />} />
       </Routes>
---
+++ frontend/src/pages/ClaimPage.jsx
@@ -0,0 +1,4 @@
+import {useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import {usePrivy, useWallets } from '@privy-io/react-auth';
+import '../styles/ClaimPage.css'
