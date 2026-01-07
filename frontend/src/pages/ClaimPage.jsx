d48f77f4d505d9964ad32b90072a2ebbf617b97d
Jeremicarose <arosejeremic5@gmail.com>
2026-01-07 23:46:12 +0300

    Minor update: 2026-01-07 23:46:12 - Modified 1 file(s)
    Changed files:
     M frontend/src/pages/ClaimPage.jsx

--- frontend/src/pages/ClaimPage.jsx
+++ frontend/src/pages/ClaimPage.jsx
@@ -1,4 +1,6 @@
 import {useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import {usePrivy, useWallets } from '@privy-io/react-auth';
-import '../styles/ClaimPage.css'
+import '../styles/ClaimPage.css';
+
+const
