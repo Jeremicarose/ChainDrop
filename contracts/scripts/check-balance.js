2e4386fc83f59d90f7703a753e0e1449459fb8b3
Jeremicarose <arosejeremic5@gmail.com>
2026-01-09 15:23:33 +0300

    Major update: 2026-01-09 15:23:33 - Added 1 file(s)
    Changed files:
    ?? contracts/scripts/check-balance.js

---
+++ contracts/scripts/check-balance.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  const balance = await hre.ethers.provider.getBalance(deployer.address);
+
+  console.log("Address:", deployer.address);
+  console.log("Balance:", hre.ethers.formatEther(balance), "TCRO");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
