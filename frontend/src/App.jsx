8846e495f0eb8221a6f9d9a10059e9c838261a37
Jeremicarose <arosejeremic5@gmail.com>
2026-01-09 08:30:00 +0300

    Minor update: 2026-01-09 08:30:00 - Added 1 file(s) - Modified 1 file(s)
    Changed files:
     M frontend/src/App.jsx
    ?? frontend/src/styles/WalletPage.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ClaimPage from './pages/ClaimPage';
 import SendPage from './pages/SendPage';
+import WalletPage from './pages/WalletPage';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/claim/:claimToken" element={<ClaimPage />} />
         <Route path="/send" element={<SendPage />} />
+        <Route path="/wallet" element={<WalletPage />} />
       </Routes>
     </Router>
   );
